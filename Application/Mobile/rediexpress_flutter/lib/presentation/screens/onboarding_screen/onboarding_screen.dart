60988373c84114ff89bad3ca77824196143adabd
EventGamer67 <78137474+EventGamer67@users.noreply.github.com>
2024-01-25 12:29:58 +0500

    qwe

--- Application/Mobile/rediexpress_flutter/lib/presentation/screens/onboarding_screen/onboarding_screen.dart
+++ Application/Mobile/rediexpress_flutter/lib/presentation/screens/onboarding_screen/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -9,7 +10,6 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
-
   late final PageController _pageController;
   @override
   void initState() {
@@ -17,47 +17,65 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     super.initState();
   }
 
-  void _pageChanged(int index){
-    
-  }
+  void _pageChanged(int index) {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Container(
-          child: Center(
-            child:
-              PageView(
-                controller: _pageController,
-                onPageChanged: (value) {
-                  _pageChanged(value);
-                },
-                children: [ 
-                  Column(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              LimitedBox(
+                maxHeight: MediaQuery.of(context).size.width,
+                child: PageView(
+                    controller: _pageController,
+                    onPageChanged: (value) {
+                      _pageChanged(value);
+                    },
                     children: [
-                      SvgPicture.asset('assets/svg/1.svg',width: MediaQuery.of(context).size.width,),
-                      const SizedBox(height: 20,)
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      SvgPicture.asset('assets/svg/2.svg',width: MediaQuery.of(context).size.width,),
-                      const SizedBox(height: 20,)
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      SvgPicture.asset('assets/svg/3.svg',width: MediaQuery.of(context).size.width,),
-                      const SizedBox(height: 20,)
-                    ],
-                  )
-                ]
+                      SvgPicture.asset(
+                        'assets/svg/1.svg',
+                      ),
+                      SvgPicture.asset(
+                        'assets/svg/2.svg',
+                      ),
+                      SvgPicture.asset(
+                        'assets/svg/3.svg',
+                      )
+                    ]),
               ),
+              Column(
+                children: [
+                  SizedBox(height: 82,),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 5, 95, 250),
+                            borderRadius: BorderRadius.all(Radius.circular(9))
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Center(child: Text('Next',style: GoogleFonts.roboto(color: Colors.white) )),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
     );
   }
 }
+
+
