31579e3979345af8bd905d974037f6402d43acf9
EventGamer67 <islamoffdanil67@gmail.com>
2024-01-26 11:18:50 +0500

    a

--- Application/Mobile/rediexpress_flutter/lib/presentation/screens/signup_screen/signup_screen.dart
+++ Application/Mobile/rediexpress_flutter/lib/presentation/screens/signup_screen/signup_screen.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -30,134 +31,151 @@ class _SignUpScreenState extends State<SignUpScreen> {
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Padding(
         padding: const EdgeInsets.all(24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              alignment: Alignment.centerLeft,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Create an account",
-                    style: GoogleFonts.roboto(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 24,
-                        color: Theme.of(context).colorScheme.inverseSurface),
-                  ),
-                  Text("Complete the sign up process to get started",
+        child: Form(
+          autovalidateMode: AutovalidateMode.always,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Create an account",
                       style: GoogleFonts.roboto(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 15,
-                        color: const Color.fromARGB(255, 167, 167, 167),
-                      ))
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            MyTextField(
-              hidable: false,
-              header: "Full name",
-              formatters: [],
-              keyboardtype: TextInputType.name,
-              hint: "Ivanov Ivan",
-              controller: _fioController,
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            MyTextField(
-              hidable: false,
-              header: "Phone number",
-              formatters: [
-                FilteringTextInputFormatter.digitsOnly,
-              ],
-              keyboardtype: TextInputType.phone,
-              hint: "+7(999)999-99-99",
-              controller: _fioController,
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            MyTextField(
-              hidable: false,
-              header: "Email adress",
-              formatters: [
-                //idk how to make it
-              ],
-              keyboardtype: TextInputType.emailAddress,
-              hint: "*********@gmail.com",
-              controller: _fioController,
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            MyTextField(
-              hidable: true,
-              header: "Password",
-              formatters: [],
-              keyboardtype: TextInputType.phone,
-              hint: "********",
-              controller: _fioController,
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            MyTextField(
-              hidable: true,
-              header: "Confirm Password",
-              formatters: [],
-              keyboardtype: TextInputType.none,
-              hint: "********",
-              controller: _fioController,
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: 24,
-                    height: 24,
-                    child: Checkbox(
-                        side: BorderSide(
-                            color: Theme.of(context).colorScheme.primary,
-                            width: 1),
-                        activeColor: Theme.of(context).colorScheme.primary,
-                        value: termCB,
-                        onChanged: (value) {
-                          setState(() {
-                            termCB = value ?? false;
-                          });
-                        }),
-                  ),
-                  const SizedBox(width: 10,),
-                  LimitedBox(
-                    maxWidth: 300,
-                    child: Expanded(
-                      child: RichText(
-                        overflow: TextOverflow.fade,
-                        textAlign: TextAlign.center,
-                        softWrap: true,
-                          text: TextSpan(
-                        text: 'By ticking this box, you agree to our',
-                        style: GoogleFonts.roboto(color: Theme.of(context).colorScheme.inverseSurface),
-                        children: <TextSpan>[
-                          TextSpan(text: ' Terms and conditions and private policy',style: GoogleFonts.roboto(color: const Color.fromARGB(255, 235, 188, 46) ))
-                        ],
-                      )),
+                          fontWeight: FontWeight.w600,
+                          fontSize: 24,
+                          color: Theme.of(context).colorScheme.inverseSurface),
                     ),
-                  )
-                ],
+                    Text("Complete the sign up process to get started",
+                        style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 15,
+                          color: const Color.fromARGB(255, 167, 167, 167),
+                        ))
+                  ],
+                ),
               ),
-            ),
-            MyButtonFilled(onClick: (){ Provider.of<ThemeProvider>(context, listen: false).switchTheme(); }, width: double.infinity, height: 45, fontSize: 16, text: "Sign Up")
-          ],
+              const SizedBox(
+                height: 24,
+              ),
+              MyTextField(
+                hidable: false,
+                header: "Full name",
+                formatters: [],
+                keyboardtype: TextInputType.name,
+                hint: "Ivanov Ivan",
+                validator: (val){},
+                controller: _fioController,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              MyTextField(
+                hidable: false,
+                header: "Phone number",
+                validator: null,
+                formatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  phoneMask
+                ],
+                keyboardtype: TextInputType.phone,
+                hint: "+7(999)999-99-99",
+                controller: _fioController,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              MyTextField(
+                hidable: false,
+                validator: (value) {
+                    if (value != null && value != "") {
+                      if (EmailValidator.validate(value!)) {
+                      } else {
+                        return 'err';
+                      }
+                    }
+                    return 'err';
+                  },
+                header: "Email adress",
+                formatters: [
+                  //idk how to make it
+                ],
+                keyboardtype: TextInputType.emailAddress,
+                hint: "*********@gmail.com",
+                controller: _fioController,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              MyTextField(
+                validator: null,
+                hidable: true,
+                header: "Password",
+                formatters: [],
+                keyboardtype: TextInputType.phone,
+                hint: "********",
+                controller: _fioController,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              MyTextField(
+                validator: null,
+                hidable: true,
+                header: "Confirm Password",
+                formatters: [],
+                keyboardtype: TextInputType.none,
+                hint: "********",
+                controller: _fioController,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 24,
+                      height: 24,
+                      child: Checkbox(
+                          side: BorderSide(
+                              color: Theme.of(context).colorScheme.primary,
+                              width: 1),
+                          activeColor: Theme.of(context).colorScheme.primary,
+                          value: termCB,
+                          onChanged: (value) {
+                            setState(() {
+                              termCB = value ?? false;
+                            });
+                          }),
+                    ),
+                    const SizedBox(width: 10,),
+                    LimitedBox(
+                      maxWidth: 300,
+                      child: Expanded(
+                        child: RichText(
+                          overflow: TextOverflow.fade,
+                          textAlign: TextAlign.center,
+                          softWrap: true,
+                            text: TextSpan(
+                          text: 'By ticking this box, you agree to our',
+                          style: GoogleFonts.roboto(color: Theme.of(context).colorScheme.inverseSurface),
+                          children: <TextSpan>[
+                            TextSpan(text: ' Terms and conditions and private policy',style: GoogleFonts.roboto(color: const Color.fromARGB(255, 235, 188, 46) ))
+                          ],
+                        )),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              MyButtonFilled(onClick: (){ Provider.of<ThemeProvider>(context, listen: false).switchTheme(); }, width: double.infinity, height: 45, fontSize: 16, text: "Sign Up")
+            ],
+          ),
         ),
       ),
     );
--- Application/Mobile/rediexpress_flutter/lib/presentation/widgets/my_textfield.dart
+++ Application/Mobile/rediexpress_flutter/lib/presentation/widgets/my_textfield.dart
@@ -1,7 +1,9 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class MyTextField extends StatefulWidget {
   final List<TextInputFormatter> formatters;
@@ -10,6 +12,7 @@ class MyTextField extends StatefulWidget {
   final bool hidable;
   final TextEditingController controller;
   final TextInputType keyboardtype;
+  final String? Function(String?)? validator;
   const MyTextField(
       {super.key,
       required this.formatters,
@@ -17,6 +20,7 @@ class MyTextField extends StatefulWidget {
       required this.keyboardtype,
       required this.hidable,
       required this.hint,
+      required this.validator,
       required this.controller});
 
   @override
@@ -25,6 +29,7 @@ class MyTextField extends StatefulWidget {
 
 class _MyTextFieldState extends State<MyTextField> {
   bool hided = false;
+  bool correct = true;
 
   @override
   void initState() {
@@ -54,52 +59,59 @@ class _MyTextFieldState extends State<MyTextField> {
         ),
         SizedBox(
           height: 40,
-          child: TextField(
-              obscureText: hided,
-              controller: widget.controller,
-              inputFormatters: widget.formatters,
-              textAlignVertical: TextAlignVertical.bottom,
-              style: GoogleFonts.roboto(),
-              keyboardType: this.widget.keyboardtype,
-              decoration: InputDecoration(
-                  suffixIcon: this.widget.hidable
-                      ? GestureDetector(
-                          onTap: () {
-                            _toggleHide();
-                          },
-                          child: Container(
-                            width: 40,
-                            height: 40,
-                            alignment: Alignment.center,
-                            child: SvgPicture.asset(
-                                hided ? 'assets/svg/vuesax/linear/eye-slash.svg' : 'assets/svg/vuesax/linear/eye.svg',
-                                alignment: Alignment.center,
-                                color:
-                                    Theme.of(context).colorScheme.inverseSurface),
-                          ))
-                      : null,
-                  errorStyle: GoogleFonts.roboto(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: Theme.of(context).colorScheme.error),
-                  errorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 1,
-                          color: Theme.of(context).colorScheme.error)),
-                  focusedBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 1, color: Color.fromARGB(255, 167, 167, 167))),
-                  hintText: this.widget.hint,
-                  hintStyle: GoogleFonts.roboto(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: const Color.fromARGB(255, 207, 207, 207)),
-                  border: const OutlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 1,
-                          color: Color.fromARGB(255, 167, 167, 167))))),
+          child: Expanded(
+            child: TextFormField(
+                obscureText: hided,
+                controller: widget.controller,
+                inputFormatters: widget.formatters,
+                textAlignVertical: TextAlignVertical.bottom,
+                style: GoogleFonts.roboto(color: correct ? Theme.of(context).colorScheme.inverseSurface :Theme.of(context).colorScheme.error ),
+                keyboardType: this.widget.keyboardtype,
+                validator: this.widget.validator,
+                decoration: InputDecoration(
+                    suffixIcon: this.widget.hidable
+                        ? GestureDetector(
+                            onTap: () {
+                              _toggleHide();
+                            },
+                            child: Container(
+                              width: 40,
+                              height: 40,
+                              alignment: Alignment.center,
+                              child: SvgPicture.asset(
+                                  hided
+                                      ? 'assets/svg/vuesax/linear/eye-slash.svg'
+                                      : 'assets/svg/vuesax/linear/eye.svg',
+                                  alignment: Alignment.center,
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .inverseSurface),
+                            ))
+                        : null,
+                    errorStyle: TextStyle(fontSize: 0),
+                    errorBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1,
+                            color: Theme.of(context).colorScheme.error)),
+                    focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1,
+                            color: Color.fromARGB(255, 167, 167, 167))),
+                    hintText: this.widget.hint,
+                    hintStyle: GoogleFonts.roboto(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: const Color.fromARGB(255, 207, 207, 207)),
+                    border: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1,
+                            color: Color.fromARGB(255, 167, 167, 167))))),
+          ),
         ),
       ],
     );
   }
 }
+
+var phoneMask = MaskTextInputFormatter(
+    mask: '+#(###)###-##-##', type: MaskAutoCompletionType.eager);
